ed3cad3a76fa2779f4b4511036cc4af0b3134dd6
Eric Bundy <elbzero@gmail.com>
2018-08-25 12:13:54 -0400

    Add Customer Table with VIP and Driving Directions columns

---
+++ app/models/box.rb
@@ -0,0 +1,11 @@
+class Box
+
+  attr_accessor :length, :width, :height
+
+   # constructor method
+   def initialize(w,h,l)
+      self.width = w
+      self.height = h
+      self.length = l
+   end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_221813) do
+ActiveRecord::Schema.define(version: 2018_08_25_160212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "driving_directions"
+    t.binary "vip"
+  end
 
   create_table "ticket_notes", force: :cascade do |t|
     t.integer "ticket_id"
