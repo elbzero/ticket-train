e4c8e34b6bf903bc6bb65aaa77ca554964b57b9e
Eric Bundy <elbzero@gmail.com>
2019-06-08 16:01:44 -0400

    Added RSPEC and WorkLog Start Time and End Time Validations

--- app/models/work_log.rb
+++ app/models/work_log.rb
@@ -1,5 +1,11 @@
 class WorkLog < ApplicationRecord
   belongs_to :ticket
+  validates :start_time,:end_time, presence: true
+  validate :start_time_valid
+
+  def start_time_valid
+    errors.add(:start_time,'Start Time CANNOT be AFTER End Time') if start_time.present? && start_time > end_time
+  end
 
   def total_time
     ((end_time - start_time) / 60 / 60).round(2)
