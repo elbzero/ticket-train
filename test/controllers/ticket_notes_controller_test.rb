1d5bc7864144b9d6b650887cd908dbba392691e3
Eric Bundy <elbzero@gmail.com>
2019-07-06 14:43:08 -0400

    remove rails test folder since we are using rspec

--- test/controllers/ticket_notes_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TicketNotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/work_log_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WorkLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
